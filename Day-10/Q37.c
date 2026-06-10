b1188df366583891282fe062761918020963528d
suryabhan8853 <surya885372@gmail.com>
2026-06-10 15:52:24 +0530

    Add files via upload

---
+++ Day-10/Q37.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main() {
+    int i,j,n;
+    printf("Enter number of row : ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        for(j=1;j<=(n-i);j++){
+            printf(" ");
+        }
+        for(j=i;j<=(2*i-1);j++){
+            printf("*");
+          }
+          printf("\n");
+        }
+    return 0;
+}
